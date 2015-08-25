f87dd670796ead6579f1800526f983a315888830
Tomasfdel <Tomasfdel@users.noreply.github.com>
2015-08-25 17:02:37 -0300

    Agregar variables Costo y cambiar PagarBoleto

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ class Tarjeta:
         self._Saldo = 0
         self._Registro= []
         self._Viaje = Viaje()
-
-    
+        self._CostoComun = 5.75
+        self._CostoTransbordo = 1.90
+        
     def Recarga (self, monto):
         if monto == 196:
             self._Saldo = self._Saldo + 230
@@ -16,28 +17,38 @@ class Tarjeta:
                 self._Saldo = self._Saldo + monto
 
 
+    def PagarBoleto (self, colectivo, horario):
+        if self.ExistenViajes:
+            UltimoViaje = self._Registro[:-1]
+            
+            
+        else:
+            if self._Saldo > self_CostoComun:
+                self._Saldo = self._Saldo - self._CostoComun
+                self._Viaje.setValores (colectivo, horario, self._CostoComun)
+                self._Registro.append(self._Viaje)
+                print("Pasaste\n")
+                return True
+            else:
+                print("No Pasaste\n")
+                return False
+
     def Saldo (self):
         return self._Saldo
-
 
     def ViajesRealizados (self):
         return self._Registro
 
-    
-    def PagarBoleto (self, colectivo, horario):
-        if self._Saldo > 5.90:
-            self._Saldo = self._Saldo - 5.90
-            self._Viaje.setValores (colectivo, horario, 5.90)
-            self._Registro.append (self._Viaje)
-            print("Pasaste\n")
+    def ExistenViajes (self):
+        if len(self._Registro) > 0:
             return True
         else:
-            print("No Pasaste\n")
             return False
+        
+
 
 
 class TarjetaMedioBoleto (Tarjeta) :
-        
     def PagarBoleto (self, colectivo, horario):
         print ("Still under development")
         return True
@@ -46,19 +57,17 @@ class TarjetaMedioBoleto (Tarjeta) :
 
 
 
-
 class Colectivo:
     def __init__ (self, empresa, linea, interno):
-        self._Empresa = empresa
-        self._Linea = linea
-        self._Interno = interno
-
+            self._Empresa = empresa
+            self._Linea = linea
+            self._Interno = interno
 
 
 class Viaje:
     def __init__ (self):
         self.setValores(0,0,0)
-        
+
     def setValores (self, colectivo, horario, monto):
         self._Colectivo = colectivo
         self._Horario = horario
